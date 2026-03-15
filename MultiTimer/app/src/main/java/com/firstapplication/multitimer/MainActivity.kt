b8e6595dd79378df2c2bae92e1526f5101b2be26
GnanasakthiT <hello.this.is.gst@gmail.com>
2026-03-15 19:00:23 +0530

    Addded the code

--- MultiTimer/app/src/main/java/com/firstapplication/multitimer/MainActivity.kt
+++ MultiTimer/app/src/main/java/com/firstapplication/multitimer/MainActivity.kt
@@ -1,47 +1,101 @@
-package com.firstapplication.multitimer
+package com.example.timerapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.firstapplication.multitimer.ui.theme.MultiTimerTheme
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private var productiveSeconds = 0
+    private var nonProductiveSeconds = 0
+    private var sleepSeconds = 0
+
+    private var productiveRunning = false
+    private var nonProductiveRunning = false
+    private var sleepRunning = false
+
+    private lateinit var handler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MultiTimerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        handler = Handler(Looper.getMainLooper())
+
+        val tvProductive = findViewById<TextView>(R.id.tvProductive)
+        val tvNonProductive = findViewById<TextView>(R.id.tvNonProductive)
+        val tvSleep = findViewById<TextView>(R.id.tvSleep)
+
+        val btnProductive = findViewById<Button>(R.id.btnProductive)
+        val btnNonProductive = findViewById<Button>(R.id.btnNonProductive)
+        val btnSleep = findViewById<Button>(R.id.btnSleep)
+        val btnReset = findViewById<Button>(R.id.btnReset)
+
+        btnProductive.setOnClickListener {
+            productiveRunning = !productiveRunning
+            if (productiveRunning) handler.post(productiveRunnable(tvProductive))
+        }
+
+        btnNonProductive.setOnClickListener {
+            nonProductiveRunning = !nonProductiveRunning
+            if (nonProductiveRunning) handler.post(nonProductiveRunnable(tvNonProductive))
+        }
+
+        btnSleep.setOnClickListener {
+            sleepRunning = !sleepRunning
+            if (sleepRunning) handler.post(sleepRunnable(tvSleep))
+        }
+
+        btnReset.setOnClickListener {
+            productiveSeconds = 0
+            nonProductiveSeconds = 0
+            sleepSeconds = 0
+            tvProductive.text = "00:00:00"
+            tvNonProductive.text = "00:00:00"
+            tvSleep.text = "00:00:00"
+            productiveRunning = false
+            nonProductiveRunning = false
+            sleepRunning = false
+        }
+    }
+
+    private fun productiveRunnable(tv: TextView): Runnable = object : Runnable {
+        override fun run() {
+            if (productiveRunning) {
+                productiveSeconds++
+                tv.text = formatTime(productiveSeconds)
+                handler.postDelayed(this, 1000)
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun nonProductiveRunnable(tv: TextView): Runnable = object : Runnable {
+        override fun run() {
+            if (nonProductiveRunning) {
+                nonProductiveSeconds++
+                tv.text = formatTime(nonProductiveSeconds)
+                handler.postDelayed(this, 1000)
+            }
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MultiTimerTheme {
-        Greeting("Android")
+    private fun sleepRunnable(tv: TextView): Runnable = object : Runnable {
+        override fun run() {
+            if (sleepRunning) {
+                sleepSeconds++
+                tv.text = formatTime(sleepSeconds)
+                handler.postDelayed(this, 1000)
+            }
+        }
+    }
+
+    private fun formatTime(seconds: Int): String {
+        val h = seconds / 3600
+        val m = (seconds % 3600) / 60
+        val s = seconds % 60
+        return String.format("%02d:%02d:%02d", h, m, s)
     }
 }
